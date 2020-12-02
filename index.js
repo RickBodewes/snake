ebfcdc0709deecaea847dac4b1fecf8b0ea2e1d7
RickBodewes <Rick@planet.nl>
2020-12-02 10:02:50 +0100

    added gold apples

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ let player = {
 }
 
 class coordinate {
-    constructor(x, y){
+    constructor(x, y) {
         this.x = x;
         this.y = y;
     }
@@ -29,9 +29,10 @@ class coordinate {
 //apple variables
 let appleLoc = new coordinate(-1, -1);
 let appleEaten = true;
+let appleIsGold = false;
 
 window.addEventListener('keydown', function (event) {
-    switch(event.keyCode){
+    switch (event.keyCode) {
         case 65:
             direction = direction == 'right' ? 'right' : 'left';
             break;
@@ -56,28 +57,33 @@ setInterval(() => {
 }, 100);
 
 //updating the entities
-function update(){
-    if(player.x == appleLoc.x && player.y == appleLoc.y){
+function update() {
+    if (player.x == appleLoc.x && player.y == appleLoc.y) {
+        if (appleIsGold) {
+            appleIsGold = false;
+            player.length += 5;
+        } else {
+            player.length++;
+        }
         appleEaten = true;
-        player.length++;
     }
 
     //checking if the array is not longer than the player.length property
-    if(player.length < player.coordinatesArray.length){
+    if (player.length < player.coordinatesArray.length) {
         player.coordinatesArray.splice(player.coordinatesArray.length - (player.coordinatesArray.length - player.length));
     }
 
     //shifting the array
-    if(player.length > 1){
-        for(let i = player.length - 1; i > 0; i--){
-            if(player.coordinatesArray[i - 1] != null){
+    if (player.length > 1) {
+        for (let i = player.length - 1; i > 0; i--) {
+            if (player.coordinatesArray[i - 1] != null) {
                 player.coordinatesArray[i] = player.coordinatesArray[i - 1];
             }
         }
     }
 
     //updating movement
-    switch (direction){
+    switch (direction) {
         case 'left':
             player.x--;
             break;
@@ -95,45 +101,49 @@ function update(){
     }
 
     //checking out of bounds
-    if(player.x > (c.width / celSize) - 1){
+    if (player.x > (c.width / celSize) - 1) {
         player.x = 0;
-    }else if(player.x < 0){
+    } else if (player.x < 0) {
         player.x = (c.width / celSize) - 1;
-    }else if(player.y > (c.height / celSize) - 1){
+    } else if (player.y > (c.height / celSize) - 1) {
         player.y = 0;
-    }else if(player.y < 0){
+    } else if (player.y < 0) {
         player.y = (c.height / celSize) - 1;
     }
-    
+
     //adding the new head to the array
     player.coordinatesArray[0] = new coordinate(player.x, player.y)
 
     //spawning the apple
-    if(appleEaten){
+    if (appleEaten) {
         appleSpawnMap = new Array(maxSnakeLength).fill(0);
-        for(let i = 0; i < player.length; i++){
-            if(player.coordinatesArray[i] != null){
+        for (let i = 0; i < player.length; i++) {
+            if (player.coordinatesArray[i] != null) {
                 appleSpawnMap[player.coordinatesArray[i].x + (player.coordinatesArray[i].y * (c.width / celSize))] = 1;
             }
         }
 
         let randPos = Math.floor(Math.random() * (maxSnakeLength - player.length)) + 1;
 
-        for(let i = 0; i < appleSpawnMap.length; i++){
-            if(appleSpawnMap[i] == 0){
+        for (let i = 0; i < appleSpawnMap.length; i++) {
+            if (appleSpawnMap[i] == 0) {
                 randPos--;
-                if(randPos == 0){
+                if (randPos == 0) {
                     appleLoc = new coordinate(i % (c.width / celSize), Math.floor(i / (c.width / celSize)));
                     appleEaten = false;
                     break;
                 }
             }
         }
+
+        if (Math.random() < .1) {
+            appleIsGold = true;
+        }
     }
 
     //checking for collision
-    for(let i = 1; i < player.length; i++){
-        if(player.coordinatesArray[0].x == player.coordinatesArray[i].x && player.coordinatesArray[0].y == player.coordinatesArray[i].y){
+    for (let i = 1; i < player.coordinatesArray.length; i++) {
+        if (player.coordinatesArray[0].x == player.coordinatesArray[i].x && player.coordinatesArray[0].y == player.coordinatesArray[i].y) {
             reset();
             break;
         }
@@ -141,15 +151,19 @@ function update(){
 }
 
 //drawing the screen
-function draw(){
+function draw() {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, c.width, c.height);
-    ctx.fillStyle = 'red';
+    if (appleIsGold) {
+        ctx.fillStyle = 'gold';
+    } else {
+        ctx.fillStyle = 'red';
+    }
     ctx.fillRect(appleLoc.x * celSize, appleLoc.y * celSize, celSize, celSize);
-    for(let i = 0; i < player.coordinatesArray.length; i++){
-        if(i == 0){
+    for (let i = 0; i < player.coordinatesArray.length; i++) {
+        if (i == 0) {
             ctx.fillStyle = 'white';
-        }else{
+        } else {
             ctx.fillStyle = `rgb(${55 + ((200 / player.length) * i)},${0},${55 + ((200 / player.length))}`;
         }
         ctx.fillRect(player.coordinatesArray[i].x * celSize, player.coordinatesArray[i].y * celSize, celSize, celSize);
@@ -157,7 +171,7 @@ function draw(){
 }
 
 //reset function
-function reset(){
+function reset() {
     player.length = 1;
     player.x = 0;
     player.y = 0;
@@ -167,7 +181,7 @@ function reset(){
 }
 
 //this is called at the beginning
-function setup(){
+function setup() {
     player.coordinatesArray.push(new coordinate(player.x, player.y));
     update();
     draw();
